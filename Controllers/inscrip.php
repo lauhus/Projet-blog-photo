b816bc4dac18b89f6738ba4e2baa9b7018c23bc7
marie.behaegel@hotmail.fr <marie.behaegel@hotmail.fr>
2020-03-31 18:16:00 +0200

    Inscription ok

--- Controllers/inscrip.php
+++ Controllers/inscrip.php
@@ -1,32 +1,28 @@
 <?php
+//var_dump($_POST);
 
 //connexion au serveur
-
-//$connexion = new PDO('mysql:host=localhost;dbname=projet_photos;charset=utf8','msb','stagiaire');
-$connexion = new PDO('mysql:host=localhost;dbname=projet_photos;charset=utf8','lauhu','stagiaire ');
-// $connexion = new PDO('mysql:host=localhost;dbname=
+$connexion = new PDO('mysql:host=localhost;dbname=projet_photos;charset=utf8','msb','stagiaire');
+//$connexion = new PDO('mysql:host=localhost;dbname=projet_photos;charset=utf8','lauhu','stagiaire ');
 
 //Création de la BDD
-
-$nom=htmlspecialchars($_POST['nom']);
-$prenom=htmlspecialchars($_POST['prenom']);
-$email=htmlspecialchars($_POST['email']);
-$mdp=htmlspecialchars($_POST['mdp']);
-$id_money=htmlspecialchars($_POST)['id_money'];
+$nom=$_POST['nom'];
+$prenom=$_POST['prenom'];
+$email=$_POST['email'];
+$mdp=$_POST['mdp'];
+$id_money=$_POST['id_money'];
 
 //Requête de création
-
-$connexion->exec("INSERT INTO user(nom, prenom, email, mdp, id_money)VALUES('$nom','$prenom','$email','$mdp','$id_money')");
-/*if ($select->rowcount() > 0){
+$select=$connexion->prepare("INSERT INTO user(nom, prenom, email, mdp, id_money)VALUES('$nom','$prenom','$email','$mdp','$id_money')");
+$select->execute();
+$response=$select->fetch();
+//print_r($response);
+if ($select->rowcount() > 0){
     session_start();
     $_SESSION['nom']=$reponse['nom'];
     $_SESSION['prenom']=$reponse['prenom'];
     $_SESSION['email']=$reponse['email'];
     $_SESSION['mdp']=$reponse['mdp'];
     $_SESSION['id_money']=$reponse['id_money'];
-}*/
-
-$reponse=$connexion->fetch();
-
-print_r($reponse);
+}
 ?>
