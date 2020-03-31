980403e7e8958526607794c79b5909c303bbde24
lauhus <laurine.hussong@hotmail.fr>
2020-03-31 17:24:30 +0200

    essai inscrip

--- Controllers/inscrip.php
+++ Controllers/inscrip.php
@@ -2,7 +2,8 @@
 
 //connexion au serveur
 
-$connexion = new PDO('mysql:host=localhost;dbname=projet_photos;charset=utf8','msb','stagiaire');
+//$connexion = new PDO('mysql:host=localhost;dbname=projet_photos;charset=utf8','msb','stagiaire');
+$connexion = new PDO('mysql:host=localhost;dbname=projet_photos;charset=utf8','lauhu','stagiaire ');
 // $connexion = new PDO('mysql:host=localhost;dbname=
 
 //Création de la BDD
@@ -15,13 +16,17 @@ $id_money=htmlspecialchars($_POST)['id_money'];
 
 //Requête de création
 
-$connexion->exec("INSERT INTO client(nom, prenom, email, mdp, id_money)VALUES('$nom','$prenom','$email','$mdp','$id_money')");
-if ($select->rowcount() > 0){
+$connexion->exec("INSERT INTO user(nom, prenom, email, mdp, id_money)VALUES('$nom','$prenom','$email','$mdp','$id_money')");
+/*if ($select->rowcount() > 0){
     session_start();
     $_SESSION['nom']=$reponse['nom'];
     $_SESSION['prenom']=$reponse['prenom'];
     $_SESSION['email']=$reponse['email'];
     $_SESSION['mdp']=$reponse['mdp'];
     $_SESSION['id_money']=$reponse['id_money'];
-}
+}*/
+
+$reponse=$connexion->fetch();
+
+print_r($reponse);
 ?>
