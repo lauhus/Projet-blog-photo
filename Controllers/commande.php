981f9317f4e163c842e8d4e5270e2b8ed0779e2d
marie.behaegel@hotmail.fr <marie.behaegel@hotmail.fr>
2020-04-01 15:48:57 +0200

    Pbm page achat

--- Controllers/commande.php
+++ Controllers/commande.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+if(empty($_SESSION['prenom']))
+    {
+        header('Location:connnex.php');
+        exit();
+    }
+    //echo "Bienvenu $_SESSION['prenom'], vous avez accès au catalogue complet ainsi qu'à l'achat des photographies."
+    //}else{ // renvoyer vers inscription ou connexion*/
+
+// NAV BAR
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,7 +22,7 @@
 <body>
     <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="../Index.html">Accueil</a>
+        <a class="navbar-brand" href="../">Accueil</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -17,28 +30,14 @@
         <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="presentation.html"> A propos de moi <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="../"> A propos de moi <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="../Controllers/achat.php"> Mes Photographies</a>
+                <a class="nav-link" href="../"> Mes Photographies</a>
             </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-            <li class="nav-item">
-                <a class="nav-link" href="formulaire_connex.html">Vous connecter</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="formulaire_inscrip.html">Vous inscrire</a>
-            </li>
-            </form>
         </div>
     </nav>
     </div>
-    <div>
-        <div>
-            <p>Présentation:</p>
-        </div>
-    </div>
-
 </body>
 </html>
